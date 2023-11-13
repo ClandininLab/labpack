7aa9dc0e8f55ee6558d8d88c116ecc5aae7d6fa5
Minseung Choi <minseung@stanford.edu>
2023-11-13 13:59:05 -0500

    include nidaqmx and labjack-ljm in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     author_email='mhturner@stanford.edu, minseung@stanford.edu',
     packages=['labpack'],
     install_requires=[
+        'nidaqmx',
+        'labjack-ljm'
         'icosphere'],
     include_package_data=True,
     zip_safe=False,
